371111571748334b67d870431083c6553a86ab85
Phil Plencner <philip_plencner@harvard.edu>
2021-12-09 11:21:33 -0500

    Changing Librarycloud call to use recordIdentifier.

--- routes/api.js
+++ routes/api.js
@@ -8,12 +8,12 @@ router.get('/', function(req, res, next) {
   
   function getItem(req, res) {
     return new Promise((resolve,reject)=>{
-      let urn = req.query.urn;
-      console.log('urn:'+urn);
+      let recordIdentifier = req.query.recordIdentifier;
+      console.log('recordIdentifier:'+recordIdentifier);
       const options = {
         hostname: 'api.lib.harvard.edu',
         port: 443,
-        path: '/v2/items.json?urn='+urn,
+        path: '/v2/items.json?recordIdentifier='+recordIdentifier,
         method: 'GET'
       };
     
@@ -21,6 +21,7 @@ router.get('/', function(req, res, next) {
       let title = '';
       let uriType = '';
       let drsFileId = '';
+      let viewerServer = 'viewer-qa.lib.harvard.edu';
       
       const LibraryCloud = https.request(options, (res) => {
     
@@ -37,8 +38,15 @@ router.get('/', function(req, res, next) {
                 console.log('more than one!');
                 Object.entries(body.items.mods).forEach(entry => {
                   const [key, value] = entry;
-                  if (value.titleInfo.hasOwnProperty('title')) {
-                    title = value.titleInfo.title;
+                  if (value.titleInfo.length) {
+                    if (value.titleInfo[0].hasOwnProperty('title')) {
+                      title = value.titleInfo[0].title;
+                    }
+                  }
+                  else {
+                    if (value.titleInfo.hasOwnProperty('title')) {
+                      title = value.titleInfo.title;
+                    }
                   }
                   if (value.hasOwnProperty('extension')) {
                     Object.entries(value.extension).forEach(entry => {
@@ -46,15 +54,30 @@ router.get('/', function(req, res, next) {
                       if (value.hasOwnProperty('DRSMetadata')) {
                         uriType = value.DRSMetadata.uriType;
                         uriType = uriType.toLowerCase();
-                        drsFileId = value.DRSMetadata.drsFileId; 
+                        if (value.DRSMetadata.hasOwnProperty('drsFileId')) {
+                          drsFileId = value.DRSMetadata.drsFileId;
+                        }
+                        else {
+                          if (value.DRSMetadata.hasOwnProperty('drsObjectId')) {
+                            uriType = 'drs';
+                            drsFileId = value.DRSMetadata.drsObjectId;
+                          }
+                        }  
                       }
                     });
                   }
                 });
               }
               else {
-                if (body.items.mods.titleInfo.hasOwnProperty('title')) {
-                  title = body.items.mods.titleInfo.title;
+                if (body.items.mods.titleInfo.length) {
+                  if (body.items.mods.titleInfo[0].hasOwnProperty('title')) {
+                    title = body.items.mods.titleInfo[0].title;
+                  }
+                }
+                else {
+                  if (body.items.mods.titleInfo.hasOwnProperty('title')) {
+                    title = body.items.mods.titleInfo.title;
+                  }
                 }
                 if (body.items.mods.hasOwnProperty('extension')) {
                   Object.entries(body.items.mods.extension).forEach(entry => {
@@ -62,7 +85,15 @@ router.get('/', function(req, res, next) {
                     if (value.hasOwnProperty('DRSMetadata')) {
                       uriType = value.DRSMetadata.uriType;
                       uriType = uriType.toLowerCase();
-                      drsFileId = value.DRSMetadata.drsFileId; 
+                      if (value.DRSMetadata.hasOwnProperty('drsFileId')) {
+                        drsFileId = value.DRSMetadata.drsFileId;
+                      }
+                      else {
+                        if (value.DRSMetadata.hasOwnProperty('drsObjectId')) {
+                          uriType = 'drs';
+                          drsFileId = value.DRSMetadata.drsObjectId;
+                        }
+                      }
                     }
                   });
                 }
@@ -73,7 +104,7 @@ router.get('/', function(req, res, next) {
               console.log('drsFileId: '+drsFileId);
             }
 
-            itemData = {'title':title, 'uriType':uriType, 'drsFileId':drsFileId};
+            itemData = {'title':title, 'uriType':uriType, 'drsFileId':drsFileId, 'viewerServer':viewerServer};
         });
       });
       
@@ -88,15 +119,18 @@ router.get('/', function(req, res, next) {
   }
     
   getItem(req,res).then((itemData)=>{
+    viewer_url = "https://"+itemData.viewerServer+"/viewer/"+itemData.uriType+"/"+itemData.drsFileId;
     res.json( 
       {
         type: itemData.uriType,
         version:"1.0",
         provider_name:"MPS Embed",
         title:itemData.title,
+        iiif_manifest:"https://iiif.lib.harvard.edu/manifests/"+itemData.uriType+":"+itemData.drsFileId,
+        viewer_url: viewer_url,
         height:400,
         width:null,
-        html:"\u003ciframe src=\"/iframe?url=https://viewer-qa.lib.harvard.edu/viewer/"+itemData.uriType+"/"+itemData.drsFileId+"\" height=\"400px\" width=\"100%\" title=\"MPS Viewer\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e\n"
+        html:"\u003ciframe src=\"/iframe?url="+viewer_url+"\" height=\"400px\" width=\"100%\" title=\"MPS Viewer\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e\n"
       }
     );
   }).catch((error)=>{
