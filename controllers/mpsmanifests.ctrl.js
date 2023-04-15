f46636e9ad4b45a39dd647d9457392f6839ad174
Katie Amaral <kathryn_amaral@harvard.edu>
2023-04-15 01:10:49 -0400

    Add mps manifests

--- controllers/mpsmanifests.ctrl.js
+++ controllers/mpsmanifests.ctrl.js
@@ -1,5 +1,27 @@
 const httpCtrl = require('./http.ctrl');
+const consoleLogger = require('../logger/logger.js').console;
 
 const mpsManifestsCtrl = {};
+
+mpsManifestsCtrl.getManifest = async (manifestUrl) => {
+
+  // Set request values that are specific to this route
+  const requestOptionsData = {
+    method: 'GET',
+    url: manifestUrl
+  };
+
+  return httpCtrl.makeRequest(requestOptionsData);
+
+};
+
+mpsManifestsCtrl.getManifestUrl = async (urn, version = '3') => {
+  consoleLogger.debug('getManifestUrl');
+  const mpsManifestBaseUrl = process.env.MPS_MANIFEST_BASEURL || `https://mps.lib.harvard.edu/iiif`;
+  const manifestUrl = `${mpsManifestBaseUrl}/${version}/${urn}`;
+  consoleLogger.debug('manifestUrl');
+  consoleLogger.debug(manifestUrl);
+  return manifestUrl
+};
 
 module.exports = mpsManifestsCtrl;
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const consoleLogger = require('../logger/logger.js').console;
 
 const legacyManifestsCtrl = require('../controllers/legacymanifests.ctrl');
+const mpsManifestsCtrl = require('../controllers/mpsmanifests.ctrl');
 
 /* GET embed from API. */
 router.get('/legacy', async function(req, res, next) {
@@ -22,49 +23,78 @@ router.get('/legacy', async function(req, res, next) {
     return res.status(500).json(result);
   }
 
-  viewer_url = "https://"+viewerServer+"/viewer/"+data.uriType+"/"+data.drsFileId;
-  
-  consoleLogger.info("api.js /legacy");
-  consoleLogger.info(viewer_url);
-  consoleLogger.info(JSON.stringify(data));
+  viewerUrl = `https://${viewerServer}/viewer/${data.uriType}/${data.drsFileId}`;
+
+  consoleLogger.debug("api.js /legacy");
+  consoleLogger.debug(viewerUrl);
+  consoleLogger.debug(JSON.stringify(data));
+  const manifestUrl = `https://iiif.lib.harvard.edu/manifests/${data.uriType}:${data.drsFileId}`;
+  consoleLogger.debug(manifestUrl);
 
   res.json( 
     {
       type: data.uriType,
-      version:"1.0",
-      provider_name:"MPS Embed",
-      title:data.title,
-      iiif_manifest:"https://iiif.lib.harvard.edu/manifests/"+data.uriType+":"+data.drsFileId,
-      viewer_url: viewer_url,
-      height:400,
-      width:null,
-      html:"\u003ciframe src=\""+viewer_url+"\" height=\"700px\" width=\"1200px\" title=\""+data.title+"\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e\n"
+      version: "1.0",
+      provider_name: "MPS Embed",
+      title: data.title,
+      iiif_manifest: manifestUrl,
+      viewerUrl: viewerUrl,
+      height: 400,
+      width: null,
+      html: "\u003ciframe src=\""+viewerUrl+"\" height=\"700px\" width=\"1200px\" title=\""+data.title+"\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e\n"
     }
   );
 
 });
 
 router.get('/mps', async function(req, res, next) {
-  
-  let data = '';
-  let viewerServer = process.env.VIEWER_SERVER;
-  let recordIdentifier = req.query.recordIdentifier;
+  consoleLogger.debug("api.js /mps");
 
-  record = await legacyManifestsCtrl.getItem(recordIdentifier);
-  data = await legacyManifestsCtrl.getData(record);
-
+  let result = {};
   
+  const viewerServer = process.env.VIEWER_SERVER;
+  const urn = req.query.urn;
+  const manifestVersion = req.query.manifestVersion || '3';
+
+  let manifestUrl, manifestResponse, manifestData;
+
+  try {
+    manifestUrl = await mpsManifestsCtrl.getManifestUrl(urn, manifestVersion);
+  } catch(e) {
+    consoleLogger.error(e);
+    result.error = e;
+    return res.status(500).json(result);
+  }
+
+  try {
+    manifestResponse = await mpsManifestsCtrl.getManifest(manifestUrl);
+    manifestData = manifestResponse.data || {};
+  } catch(e) {
+    consoleLogger.error(e);
+    result.error = e;
+    return res.status(500).json(result);
+  }
+
+  viewerUrl = `https://${viewerServer}/viewer/mps/${urn}`;
+  
+  consoleLogger.debug("api.js /mps");
+  consoleLogger.debug(viewerUrl);
+  consoleLogger.debug(manifestUrl);
+  //consoleLogger.debug(JSON.stringify(manifestData));
+
+  const title = manifestData.id || '';
+
   res.json( 
     {
-      type: data.uriType,
-      version:"1.0",
-      provider_name:"MPS Embed",
-      title:data.title,
-      iiif_manifest:"https://iiif.lib.harvard.edu/manifests/"+data.uriType+":"+data.drsFileId,
-      viewer_url: viewer_url,
-      height:400,
-      width:null,
-      html:"\u003ciframe src=\""+viewer_url+"\" height=\"700px\" width=\"1200px\" title=\""+data.title+"\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e\n"
+      type: "mps",
+      version: "1.0",
+      provider_name: "MPS Embed",
+      title: title,
+      iiif_manifest: manifestUrl,
+      viewerUrl: viewerUrl,
+      height: 400,
+      width: null,
+      html: "\u003ciframe src=\""+viewerUrl+"\" height=\"700px\" width=\"1200px\" title=\""+title+"\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e\n"
     }
   );
 
