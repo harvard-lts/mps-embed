f8cae32f2d587e4172c9c299e773bc823cc3dbeb
Phil Plencner <40801910+phil-plencner-hl@users.noreply.github.com>
2023-07-17 21:42:42 +0200

    Title fixed for both v2 and v3 mps manifests. (#28)
    * Fixing title for mps manifests.
    
    * Removing extra line break.
    
    * Title fixed for both v2 and v3 mps manifests.

--- routes/api.js
+++ routes/api.js
@@ -88,8 +88,16 @@ router.get('/mps', async function(req, res, next) {
   viewerUrl.searchParams.append("manifestId", manifestId);
   consoleLogger.debug(viewerUrl);
 
-  const title = manifestData.id || '';
-
+  let title = manifestData.id || ''; 
+  if (manifestData.hasOwnProperty('label')) {
+    if (manifestData.label.hasOwnProperty('none')) {
+      title = manifestData.label.none[0] || '';
+    } 
+    else {
+      title = manifestData.label || '';
+    }
+  }
+  
   res.json( 
     {
       type: "mps",
