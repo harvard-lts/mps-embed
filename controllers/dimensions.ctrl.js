f9da7ce98bc86b90709b3692e68cd1ba41d680c4
Phil Plencner <philip_plencner@harvard.edu>
2023-07-21 16:16:12 -0400

    Adding in ability to modify width and height of the iframe.

---
+++ controllers/dimensions.ctrl.js
@@ -0,0 +1,15 @@
+const consoleLogger = require('../logger/logger.js').console;
+
+const dimensionsCtrl = {};
+
+dimensionsCtrl.getDimension = (dimensionParameter, defaultValue) => {
+    let currentDimension = defaultValue;
+    if (dimensionParameter) {
+        if (Number.isInteger(parseInt(dimensionParameter))) {
+            currentDimension = parseInt(dimensionParameter);
+        }
+    }
+    return currentDimension;
+}
+
+module.exports = dimensionsCtrl;
